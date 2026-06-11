edeeed623c443a95a743f84ffa9aa519a6a6443f
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:06:47 +0300

    Add files via upload

---
+++ work_3.py
@@ -0,0 +1,60 @@
+#17
+# import math
+# x1=int(input("x1="))
+# y1=int(input("y1="))
+# x2=int(input("x2="))
+# y2=int(input("y2="))
+# x3=int(input("x3="))
+# y3=int(input("y3="))
+# print("AB=", math.sqrt((x2-x1)**2+(y2-y1)**2))
+# print("AC=", math.sqrt((x3-x1)**2+(y3-y1)**2))
+# print("BC=", math.sqrt((x3-x2)**2+(y3-y2)**2))
+# print("perimetr P=", math.sqrt((x2-x1)**2+(y2-y1)**2)+
+#       math.sqrt((x3-x1)**2+(y3-y1)**2)+
+#       math.sqrt((x3-x2)**2+(y3-y2)**2))
+ #18
+# A=int(input("A="))
+# B=int(input("B="))
+# C=int(input("C="))
+# print("AC*BC=", (C-A)*(B-C))
+#19
+# x1=int(input("x1="))
+# x2=int(input("x2="))
+# y1=int(input("y1="))
+# y2=int(input("y2="))
+# print("perimetri P=", 2*(y2-y1)*(x2-x1))
+# print("yuzasi S=", (y2-y1)*(x2-x1))
+#20
+# import math
+# x1=int(input("x1="))
+# x2=int(input("x2="))
+# y1=int(input("y1="))
+# y2=int(input("y2="))
+# print("masofa h=", math.sqrt((x2-x1)**2+(y2-y1)**2))
+#21
+# import math
+# x1=int(input("x1="))
+# x2=int(input("x2="))
+# x3=int(input("x3="))
+# y1=int(input("y1="))
+# y2=int(input("y2="))
+# y3=int(input("y3="))
+# a=math.sqrt((x2-x1)**2+(y2-y1)**2)
+# b=math.sqrt((x3-x2)**2+(y3-y2)**2)
+# c=math.sqrt((x3-x2)**2+(y3-y2)**2)
+# p=a+b+c
+# s=math.sqrt(p/2*(p/2-a)*(p/2-b)*(p/2-c))
+# print("Perimetri P=", p)
+# print("Yuzasi S=", s)
+#22
+# a,b=map(int, input().split())
+# b,a=a, b
+# print(a,b)
+#23
+# a, b, c=map(int, input().split())
+# b, c, a=a, b, c
+# print(a, b, c)
+#24
+# a, b, c=map(int, input().split())
+# c, a, b=a, b, c
+# print(a,b,c)
